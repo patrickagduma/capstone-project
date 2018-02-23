04e9b092579a7f8fa92c0d57b8323d13246258dc
Patrick Agduma <patrickagduma@gmail.com>
2018-02-23 23:00:19 +0800

    fix admin student search

--- adminStudentSearch.php
+++ adminStudentSearch.php
@@ -2,7 +2,7 @@
 	require_once('libs/connect.php');
 	session_start();
 	$searchKey = $_POST['searchKey'];
-	$courseId = $_POST['course'];
+	$courseId = $_GET['cid'];
 	if (strlen($searchKey) >= 3) {
 		$sql = "SELECT u.* FROM users u WHERE lower(concat(u.firstname, ' ', u.lastname, ' ', u.email)) like  '%" . $searchKey . "%' and u.user_type='S' ";
 		$sql .= " and u.id not in ( SELECT student_id FROM student_classes WHERE class_id = " . $courseId . " ) ";
@@ -12,7 +12,7 @@
 			mysqli_close($con);
 		}
 	} else {
-		header('Location:adminSearchStudent.php?cid=' . $courseId);
+		header('Location:adminSearchStudent.php?cid=' . $courseId . '&error=search_key_too_few');
 	}
 
 
