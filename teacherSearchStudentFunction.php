92612e87e9e738b93b969c749d21a32fd2ffe692
Patrick Agduma <patrickagduma@gmail.com>
2018-02-23 22:52:36 +0800

    label changes .. naay uban label sa subject page na nakabutang classes. Fix sad uban error sa searching for student in admin page.

--- teacherSearchStudentFunction.php
+++ teacherSearchStudentFunction.php
@@ -2,17 +2,17 @@
 	require_once('libs/connect.php');
 	session_start();
 	$searchKey = $_POST['searchKey'];
-	$courseId = $_POST['course'];
+	$courseId = $_GET['cid'];
+
 	if (strlen($searchKey) >= 3) {
 		$sql = "SELECT u.* FROM users u WHERE lower(concat(u.firstname, ' ', u.lastname, ' ', u.email)) like  '%" . $searchKey . "%' and u.user_type='S' ";
 		$sql .= " and u.id not in ( SELECT student_id FROM student_classes WHERE class_id = " . $courseId . " ) ";
-
 		if($result = mysqli_query($con, $sql)){
 			include('pages/teacherSearchStudent.html');
 			mysqli_close($con);
 		}
 	} else {
-		header('Location:teacherSearchStudent.php');
+		header('Location:teacherSearchStudent.php?cid=' . $courseId . '&error=search_key_too_few');
 	}
 
 
