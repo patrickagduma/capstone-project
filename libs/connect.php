e061ef89e95f973f9ab5fa75c087d03123280212
Niko Jay <nikojaym@gmail.com>
2018-02-27 20:07:21 +0800

    kalinturahon sok

--- libs/connect.php
+++ libs/connect.php
@@ -1,20 +1,8 @@
 <?php
-	$servername = "localhost:3306";
-	$usename = "importli_cult";
-	$password = "1qaz2wsx3edc";
-	$dbname = "importli_culturama"; 
-
-	//create connection
-	$con = mysqli_connect($servername, $usename, $password, $dbname);
-
-	//check connection
-	if(!$con){
-		die('Connection fail: '.mysqli_connect_error());
-	}
-	// $servername = "localhost";
-	// $usename = "root";
-	// $password = "";
-	// $dbname = "capstone_db"; 
+	// $servername = "localhost:3306";
+	// $usename = "importli_cult";
+	// $password = "1qaz2wsx3edc";
+	// $dbname = "importli_culturama"; 
 
 	// //create connection
 	// $con = mysqli_connect($servername, $usename, $password, $dbname);
@@ -23,4 +11,16 @@
 	// if(!$con){
 	// 	die('Connection fail: '.mysqli_connect_error());
 	// }
+	$servername = "localhost";
+	$usename = "root";
+	$password = "";
+	$dbname = "capstone_db"; 
+
+	//create connection
+	$con = mysqli_connect($servername, $usename, $password, $dbname);
+
+	//check connection
+	if(!$con){
+		die('Connection fail: '.mysqli_connect_error());
+	}
 ?>
